4281dd19bd668c409330ace6e0548ccbaef15221
Blanca Marquez <blmarquez@csumb.edu>
2019-03-27 06:05:34 +0000

    adding changes to the dbConnection to make it work with the Heroku database

--- Labs/OtterMart/dbConnection.php
+++ Labs/OtterMart/dbConnection.php
@@ -3,9 +3,17 @@
 $dbname = 'ottermart';// default dbname
 function getDatabaseConnection($dbname){
     $host = 'localhost'; //cloud 9
-    $username = 'blancamarquez';
+    $username = 'root';
     $password = '';
     
+    if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
     $dbConn = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
